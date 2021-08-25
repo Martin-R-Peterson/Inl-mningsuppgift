7d67ee9d492ed89b9be1fd3228eeec0cc1ae0d40
Martin-R-Peterson <72917357+Martin-Peterson@users.noreply.github.com>
2021-08-25 15:46:29 +0200

    added index

--- Inlämningsuppgift/Pages/Index.cshtml.cs
+++ Inlämningsuppgift/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inlämningsuppgift.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +12,32 @@ namespace Inlämningsuppgift.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _dbcontext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbcontext = dbContext;
         }
+
+        public class CategoryItem
+        {
+            public string Name { get; set; }
+        }
+
+        public List<CategoryItem> Lista { get; set; }
 
         public void OnGet()
         {
+            //var lista = _dbcontext.Categories.ToList();
 
+            Lista = new List<CategoryItem>();
+            foreach (var cat in _dbcontext.Categories)
+            {
+                Lista.Add(new CategoryItem { Name = cat.Namn });
+            }
         }
     }
 }
